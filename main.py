7ce0412c2baccf7235e294d94bc6bc81a39fe306
Sung Kim <ge72bob@mytum.de>
2023-08-26 16:26:43 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
+load_dotenv()
+from langchain.llms import OpenAI
 #we have the open_api_key in the .env file. We use the dotenv.
 # llm = OpenAI(openai_api_key="...")
-load_dotenv()
 #Frontend Streamlit https://docs.streamlit.io/library/api-reference
 import streamlit as st 
 #llm model
-from langchain.llms import OpenAI
 #chat model
 from langchain.chat_models import ChatOpenAI
 
@@ -19,6 +19,9 @@ chat_model = ChatOpenAI()
 # result2=chat_model.predict("hi!")
 # # print(result2)
 
+
+# Streamlit 
+# Run "streamlit run main.py"
 st.title('AI POET')
 theme = st.text_input('Enter the theme of the poet')
 
